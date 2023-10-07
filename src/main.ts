64ba5ac6e9b638a43f33b4468d49a9e4dade1bd9
Sevi <correosevi@gmail.com>
2023-10-07 20:23:20 +0100

    Feat | Draw piece and loop through piece

--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,6 @@ canvas.height = BLOCK_SIZE * BOARD_HEIGHT;
 
 context.scale(BLOCK_SIZE, BLOCK_SIZE);
 
-// Game loop
-function update() {
-  draw();
-  window.requestAnimationFrame(update);
-}
-
-function draw() {
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-update();
-
 // Board
 const board = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -56,5 +43,46 @@ const board = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
 ];
+
+// Pieces
+const piece = {
+  position: { x: 5, y: 5 },
+  shape: [
+    [1, 1],
+    [1, 1],
+  ],
+};
+// Game loop
+function update() {
+  draw();
+  window.requestAnimationFrame(update);
+}
+
+function draw() {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Loop through board
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        context.fillStyle = '#fff';
+        context.fillRect(x, y, 1, 1);
+      }
+    });
+  });
+
+  // Loop through piece
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value) {
+        context.fillStyle = 'red';
+        context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1);
+      }
+    });
+  });
+}
+
+update();
