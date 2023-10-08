0a5d27fcf050494d7cb06cff15a2177f322f3489
Sevi <correosevi@gmail.com>
2023-10-08 20:15:10 +0100

    Feat | Create solidify function

--- src/main.ts
+++ src/main.ts
@@ -95,6 +95,19 @@ function checkCollision() {
   });
 }
 
+function solidifyPiece() {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        board[y + piece.position.y][x + piece.position.x] = 1;
+      }
+    });
+  });
+
+  piece.position.x = 0;
+  piece.position.y = 0;
+}
+
 document.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowLeft') {
     piece.position.x--;
@@ -112,6 +125,7 @@ document.addEventListener('keydown', (event) => {
     piece.position.y++;
     if (checkCollision()) {
       piece.position.y--;
+      solidifyPiece();
     }
   }
 });
