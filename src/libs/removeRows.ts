2811fe1b6faf343d1f7e8f0bf4556375fe95629d
Sevi <correosevi@gmail.com>
2023-10-21 09:24:26 +0100

    Feat | Extract updateScore function

--- src/libs/removeRows.ts
+++ src/libs/removeRows.ts
@@ -1,5 +1,6 @@
-import { CANVAS_CONFIG, SCORE_CONFIG } from '../const';
+import { CANVAS_CONFIG } from '../const';
 import { state } from '../globalStates';
+import updateScore from './updateScore';
 
 export default function checkAndRemoveRows(board: any) {
   const fullRows = [];
@@ -21,21 +22,5 @@ export default function checkAndRemoveRows(board: any) {
   });
 
   state.totalLinesRemoved += linesRemoved;
-
-  switch (linesRemoved) {
-    case 1:
-      state.score += SCORE_CONFIG.ONE_ROW;
-      break;
-    case 2:
-      state.score += SCORE_CONFIG.TWO_ROWS;
-      break;
-    case 3:
-      state.score += SCORE_CONFIG.THREE_ROWS;
-      break;
-    case 4:
-      state.score += SCORE_CONFIG.FOUR_ROWS;
-      break;
-    default:
-      break;
-  }
+  updateScore(linesRemoved);
 }
