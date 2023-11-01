3be0a4cb60d89ec1d785c855eda646f61749aef0
Sevi <correosevi@gmail.com>
2023-11-01 20:17:54 +0000

    Feat | Extract load controler for more readable code

--- src/libs/handlers/handleGamePadEvents.ts
+++ src/libs/handlers/handleGamePadEvents.ts
@@ -16,65 +16,116 @@ let greenPressed = false;
 let bluePressed = false;
 
 export function handleGamePad({ nextPieceCanvas, nextPieceContext }: any) {
+  const gamePad = loadControler();
+
+  if (gamePad) {
+    const {
+      DownArrow,
+      LeftArrow,
+      RightArrow,
+      greenButton,
+      blueButton,
+      pauseButton,
+    } = gamePad;
+
+    // Right arrow
+    handleGamePadPushRight(RightArrow);
+    // Left arrow
+    handleGamePadPushLeft(LeftArrow);
+
+    // Down arrow (Soft push)
+    handleGamePadPushDown(DownArrow, nextPieceCanvas, nextPieceContext);
+
+    // Green button (Rotation)
+    handleGamePadPushGreen(greenButton);
+
+    // Blue button (Hard push)
+    handleGamePadPushBlueButton(blueButton, nextPieceCanvas, nextPieceContext);
+
+    // Pause button
+    handleGamePadPause(pauseButton);
+  }
+}
+
+function loadControler() {
   let controllerIndex = states.getControllerIndex();
 
   if (controllerIndex !== null) {
     const gamepad = navigator.getGamepads()[controllerIndex];
 
     if (gamepad) {
-      // Extract the buttons
-      const buttons = gamepad.buttons;
       // Check if the buttons are pressed
-      const DownArrow = buttons[13].pressed;
-      const LeftArrow = buttons[14].pressed;
-      const RightArrow = buttons[15].pressed;
-      const greenButton = buttons[0].pressed;
-      const blueButton = buttons[2].pressed;
-      const pauseButton = buttons[9].pressed;
+      const DownArrow = gamepad.buttons[13].pressed;
+      const LeftArrow = gamepad.buttons[14].pressed;
+      const RightArrow = gamepad.buttons[15].pressed;
+      const greenButton = gamepad.buttons[0].pressed;
+      const blueButton = gamepad.buttons[2].pressed;
+      const pauseButton = gamepad.buttons[9].pressed;
 
-      // Right arrow
-      if (RightArrow && Date.now() - lastMoveTime > moveInterval) {
-        handleRightMovement();
-        lastMoveTime = Date.now();
-      }
-
-      // Left arrow
-      if (LeftArrow && Date.now() - lastMoveTime > moveInterval) {
-        handleLeftMovement();
-        lastMoveTime = Date.now();
-      }
-
-      // Down arrow (Soft push)
-      if (DownArrow && Date.now() - lastMoveTime > downMoveInterval) {
-        handleDownMovement(() =>
-          solidifyPiece({ nextPieceCanvas, nextPieceContext }),
-        );
-        lastMoveTime = Date.now();
-      }
-
-      // Green button (Rotation)
-      if (greenButton && !greenPressed) {
-        greenPressed = true;
-        handleRotation();
-      } else if (!greenButton) {
-        greenPressed = false;
-      }
-
-      // Blue button (Hard push)
-      if (blueButton && !bluePressed) {
-        bluePressed = true;
-        handleHardPush(() =>
-          solidifyPiece({ nextPieceCanvas, nextPieceContext }),
-        );
-      } else if (!blueButton) {
-        bluePressed = false;
-      }
-
-      // Pause button
-      if (pauseButton && Date.now() - lastMoveTime > hardPushInterval) {
-        states.setIsPaused(!states.getIsPaused());
-        lastMoveTime = Date.now();
-      }
+      return {
+        DownArrow,
+        LeftArrow,
+        RightArrow,
+        greenButton,
+        blueButton,
+        pauseButton,
+      };
     }
+  }
+}
+
+function handleGamePadPushLeft(LeftArrow: any) {
+  if (LeftArrow && Date.now() - lastMoveTime > moveInterval) {
+    handleLeftMovement();
+    lastMoveTime = Date.now();
+  }
+}
+
+function handleGamePadPushRight(RightArrow: any) {
+  if (RightArrow && Date.now() - lastMoveTime > moveInterval) {
+    handleRightMovement();
+    lastMoveTime = Date.now();
+  }
+}
+
+function handleGamePadPushDown(
+  DownArrow: any,
+  nextPieceCanvas: any,
+  nextPieceContext: any,
+) {
+  if (DownArrow && Date.now() - lastMoveTime > downMoveInterval) {
+    handleDownMovement(() =>
+      solidifyPiece({ nextPieceCanvas, nextPieceContext }),
+    );
+    lastMoveTime = Date.now();
+  }
+}
+
+function handleGamePadPushGreen(greenButton: any) {
+  if (greenButton && !greenPressed) {
+    greenPressed = true;
+    handleRotation();
+  } else if (!greenButton) {
+    greenPressed = false;
+  }
+}
+
+function handleGamePadPushBlueButton(
+  blueButton: any,
+  nextPieceCanvas: any,
+  nextPieceContext: any,
+) {
+  if (blueButton && !bluePressed) {
+    bluePressed = true;
+    handleHardPush(() => solidifyPiece({ nextPieceCanvas, nextPieceContext }));
+  } else if (!blueButton) {
+    bluePressed = false;
+  }
+}
+
+function handleGamePadPause(pauseButton: any) {
+  if (pauseButton && Date.now() - lastMoveTime > hardPushInterval) {
+    states.setIsPaused(!states.getIsPaused());
+    lastMoveTime = Date.now();
   }
 }
